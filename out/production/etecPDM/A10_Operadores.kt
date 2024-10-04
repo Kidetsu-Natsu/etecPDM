bd5dd75fbc5f5003cdb96eaa550e2be6bda94cf3
Luis Henrik M. Teixeira <luis.teixeira32@etec.sp.gov.br>
2024-10-04 18:17:38 -0300

    Add files via upload

---
+++ out/production/etecPDM/A10_Operadores.kt
@@ -0,0 +1,20 @@
+//Aula 10: Operadoers de comparação e igualdade
+//operadores Lógicos e Operadores de intervalo
+
+fun main()
+{
+    var n = 7
+
+    if(n > 5) println("$n é maior que 5")
+    if(n < 9) println("$n é menor que 9")
+    if(n >= 6) println("$n é maior ou igual a 6")
+    if(n <= 7) println("$n é menor ou igual a 7")
+
+    if(n == 7) println("$n é igual a 7")
+    if(n != 4) println("$n é diferente de 4")
+    if(!(n == 10)) println("$n não é igual 10")
+
+
+}
+
+
